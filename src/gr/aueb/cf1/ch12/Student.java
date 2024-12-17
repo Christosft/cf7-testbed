34816f130682b8afd00e90addaf08551c682c4c2
Chris Ftoulis <christosftoulis3@gmail.com>
2024-12-17 21:34:13 +0200

    initial commit

---
+++ src/gr/aueb/cf1/ch12/Student.java
@@ -0,0 +1,7 @@
+package gr.aueb.cf1.ch12;
+
+public class Student {
+    int id;
+    String firstname = "Chris";
+    String lastname;
+}
---
+++ src/gr/aueb/cf1/ch12/StudentDemo.java
@@ -0,0 +1,8 @@
+package gr.aueb.cf1.ch12;
+
+public class StudentDemo {
+
+    public static void main(String[] args) {
+        Student alice = new Student();
+    }
+}
