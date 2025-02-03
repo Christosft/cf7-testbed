1a26cadb2f4c860b82e7de08a0845908ecb5362f
Chris Ftoulis <christosftoulis3@gmail.com>
2025-02-03 21:09:06 +0200

    chapter 19 Generics Doubly list created

---
+++ src/gr/aueb/cf1/ch19/doubly_linked_list/DoublyLinkedList.java
@@ -0,0 +1,37 @@
+package gr.aueb.cf1.ch19.doubly_linked_list;
+
+public class DoublyLinkedList<T> {
+    private Node<T> head = null;
+    private Node<T> tail = null;
+
+
+    /**
+     * time complexity 0(1)
+     *
+     * @param t the value to be inserted
+     */
+    public void insertFirst(T t) {
+        Node<T> tmp = new Node<>();
+        tmp.setItem(t);
+        tmp.setNext(head);
+        tmp.setPrev(null);
+
+        if (head == null) tail = tmp;
+        else head.setPrev(tmp);
+
+        head = tmp;
+    }
+
+    public void insertLast(T t) {
+        if (head == null) {
+            insertFirst(t);
+            return;
+        }
+
+
+        Node<T> tmp = new Node<>();
+        tmp.setItem(t);
+        tmp.setNext(head);
+        tmp.setPrev(tail);
+    }
+}
