b5ecf5d89ee26e711575e86be6678c3e557da021
Chris Ftoulis <christosftoulis3@gmail.com>
2025-03-18 19:59:37 +0200

    feat : "chapter 26 created"

---
+++ src/gr/aueb/cf1/ch26/ConcurrentDateTimeServer.java
@@ -0,0 +1,28 @@
+package gr.aueb.cf1.ch26;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+import java.util.Date;
+
+public class ConcurrentDateTimeServer implements Runnable {
+
+    private final Socket socket;
+
+    public ConcurrentDateTimeServer(Socket socket) {
+        this.socket = socket;
+    }
+
+    public void run() {
+        try (BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()))) {
+            bw.write(new Date().toString());
+            bw.flush();
+            socket.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
